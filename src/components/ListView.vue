af8cb0efb1c936173165743181bb51175684a6a6
sean <infocentre@naver.com>
2018-02-13 09:24:17 +1100

    double click editable

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
     <div v-for="word in list">
-      <input type="text" v-model="word.word" @blur="changeWordDone(word)" @keyup.enter="changeWordDone(word)">
-      <span @dblclick="changeWord(word)">{{word.word}}</span>
-      <input type="text" v-model="word.translation" @blur="changeWordDone(word)" @keyup.enter="changeWordDone(word)">
-      <span>{{word.translation}}</span>
+      <input type="text" v-model="word.word" class="wordInput" :class="{showWord:word == editWord && 'wordword'== element}" @blur="changeWordDone(word)" @keyup.enter="changeWordDone(word)">
+      <span @dblclick="changeWord(word, $event)" class="wordword">{{word.word}}</span>
+      <input type="text" v-model="word.translation" class="wordInput" :class="{showWord:word == editWord && 'tran' == element}" @blur="changeWordDone(word)" @keyup.enter="changeWordDone(word)">
+      <span @dblclick="changeWord(word, $event)" class="tran" >{{word.translation}}</span>
       <button @click="removeWord(word)">X</button>
     </div>
   </div>
 </template>
+
+<style>
+.wordInput {
+  display: none
+}
+.showWord {
+  display: inline
+}
+</style>
+
 <script>
 import firebase from '../service/firebase'
 export default {
   data () {
     return {
       list: this.$store.state.list,
-      uid: this.$store.state.uid
+      uid: this.$store.state.uid,
+      editWord: null,
+      element: null
     }
   },
   methods: {
@@ -30,11 +42,15 @@ export default {
         window.vm.$store.state.list = arr
       })
     },
-    changeWord: function (word) {
-      console.log(word)
+    changeWord: function (word, event) {
+      console.log(event.target.className)
+      this.element = event.target.className
+      this.editWord = word
     },
     changeWordDone: function (word) {
       firebase.init.database().ref('memoword').child(word.id).update(word)
+      this.editWord = null
+      this.element = null
       console.log(word)
     },
     removeWord: function (word) {
